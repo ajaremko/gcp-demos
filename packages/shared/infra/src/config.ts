0d41ec6d899bbd0b178706540ef3173b346c0c48
ajaremko <alfredsyoung@gmail.com>
2026-08-19 08:04:16 +0000

    remove prefixes from shared-infra config files

--- packages/shared/infra/src/config.ts
+++ packages/shared/infra/src/config.ts
@@ -5,14 +5,12 @@ export const stackSuffix = stackName.toUpperCase()
 
 export const tag = 'shared'
 
-const coreConfig = new pulumi.Config('core')
-export const gcpProject = coreConfig.require('project')
-export const gcpRegion = coreConfig.require('region')
-export const githubOrg = coreConfig.require('githubOrg')
-export const githubRepo = coreConfig.require('githubRepo')
-export const workloadIdentityPoolId = coreConfig.require(
-  'workloadIdentityPoolId',
-)
+const config = new pulumi.Config()
+export const gcpProject = config.require('project')
+export const gcpRegion = config.require('region')
+export const githubOrg = config.require('githubOrg')
+export const githubRepo = config.require('githubRepo')
+export const workloadIdentityPoolId = config.require('workloadIdentityPoolId')
 
 export const coreLabels: Record<string, string> = {
   env: stackName,
