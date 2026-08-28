a32a3c4694fbd9f713f22dd6c6270d431d6751ad
ajaremko <alfredsyoung@gmail.com>
2026-08-28 22:42:48 +0000

    debug cms env vars

--- packages/blog/cms/run.sh
+++ packages/blog/cms/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-litestream restore -if-replica-exists -o /data/blog.db "gcs://$GCS_MEDIA_BUCKET/db/blog.db"
+litestream restore -if-replica-exists -o /data/blog.db "gcs://$GCS_DATA_BUCKET/db/blog.db"
 npx payload migrate
 exec litestream replicate -exec "node packages/blog/cms/server.js"
