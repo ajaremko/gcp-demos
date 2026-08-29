a92a4fbd4df52a50c2b772ef467f19e41f3b11b7
ajaremko <alfredsyoung@gmail.com>
2026-08-29 01:50:43 +0000

    debug run script

--- packages/blog/cms/run.sh
+++ packages/blog/cms/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+echo "Running with /etc/litestream.yml:"
+cat /etc/litestream.yml
 set -e
 litestream restore -if-replica-exists -o /data/blog.sqlite "gcs://$GCS_DATA_BUCKET/blog.sqlite"
 npx payload migrate
-echo "Running with /etc/litestream.yml:"
-cat /etc/litestream.yml
 exec litestream replicate -exec "node packages/blog/cms/server.js"
