b68c9b92cf2ece762f27f3e6faf1d93f2314b841
ajaremko <alfredsyoung@gmail.com>
2026-09-01 19:19:06 +0000

    remove run script from cms, add src code and payload config env to cms docker image

--- packages/blog/cms/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Running with /etc/litestream.yml:"
-cat /etc/litestream.yml
-set -e
-litestream restore -if-replica-exists -o /data/blog.sqlite "gcs://$GCS_DATA_BUCKET/blog.sqlite"
-npx payload migrate
-exec litestream replicate -exec "node packages/blog/cms/server.js"
